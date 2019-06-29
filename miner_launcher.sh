5fa8709c8cc2084f95af527609e4a5deade7f83c
infinityemperor <52202818+infinityemperor@users.noreply.github.com>
2019-06-29 18:28:50 +0500

    Update miner_launcher.sh

--- miner_launcher.sh
+++ miner_launcher.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-    DEFAULT_DELAY=0
-    if [ "x$1" = "x" -o "x$1" = "xnone" ]; then
-       DELAY=$DEFAULT_DELAY
-    else
-       DELAY=$1
-    fi
-    sleep $DELAY
-    cd /usr/local/claymore
-    su emperor -c "screen -dmS ethm ./start.bash"
-    su emperor -c "screen -dmS ethm ./start1.bash"
+DEFAULT_DELAY=0
+if [ "x$1" = "x" -o "x$1" = "xnone" ]; then
+   DELAY=$DEFAULT_DELAY
+else
+   DELAY=$1
+fi
+sleep $DELAY
+cd /usr/local/claymore/claymore14
+su emperor -c "screen -dmS ethm ./start.bash"
+su emperor -c "screen -dmS ethm ./start1.bash"
